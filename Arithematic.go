bff9f415735114e3e4fbfb6aef87bd23249190ba
Nannapaneni <sita.nannapaneni@capgemini.com>
2023-01-17 12:50:43 +0530

    new file added

---
+++ Arithematic.go
@@ -0,0 +1,54 @@
+package main
+import "fmt"
+func addNumber(a, b int) int {
+	return a + b
+}
+func sub(a, b int) int {
+	return a - b
+}
+func mul(a, b int) int {
+	return a * b
+}
+func division(a, b int) int {
+	return a / b
+}
+func main() {
+	var a, b, c, d, e, f int
+	a = 18
+	b = 9
+	c = addNumber(a, b)
+	d = sub(a, b)
+	e = mul(a, b)
+	f = division(a, b)
+	fmt.Println("The addition is: ", c)
+	fmt.Println("The substrion is: ", d)
+	fmt.Println("The multiplacation is: ", e)
+	fmt.Println("The Division is: ", f)
+}
+package main
+import "fmt"
+func addNumber(a, b int) int {
+	return a + b
+}
+func sub(a, b int) int {
+	return a - b
+}
+func mul(a, b int) int {
+	return a * b
+}
+func division(a, b int) int {
+	return a / b
+}
+func main() {
+	var a, b, c, d, e, f int
+	a = 18
+	b = 9
+	c = addNumber(a, b)
+	d = sub(a, b)
+	e = mul(a, b)
+	f = division(a, b)
+	fmt.Println("The addition is: ", c)
+	fmt.Println("The substrion is: ", d)
+	fmt.Println("The multiplacation is: ", e)
+	fmt.Println("The Division is: ", f)
+}
